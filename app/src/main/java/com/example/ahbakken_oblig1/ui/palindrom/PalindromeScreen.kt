3f509e96557eb8fa4fcbe5f0f8c87be45f0a61b4
Astrid Bakken <astridbakken95@gmail.com>
2023-02-20 19:56:08 +0100

    Fix: Put function into the PalindromeScreen, change clickable cards to buttons, Anna is palindrome

--- app/src/main/java/com/example/ahbakken_oblig1/ui/palindrom/PalindromeScreen.kt
+++ app/src/main/java/com/example/ahbakken_oblig1/ui/palindrom/PalindromeScreen.kt
@@ -1,8 +1,8 @@
 package com.example.ahbakken_oblig1.ui.palindrom
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material3.*
@@ -12,59 +12,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.ahbakken_oblig1.ui.theme.Ahbakken_oblig1Theme
 
 @Composable
 fun PalindromeScreen(modifier: Modifier, onNavigateToNext: () -> Unit) { //is called in main
     Column (
-        modifier = modifier.padding(75.dp),
+        modifier = modifier.padding(top = 50.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
-    ){
-        PalindromeTest()
-    }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom
-    ){
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable{ },
-            onClick = {
-                onNavigateToNext()
-            }
-        ) {
-            Row (
-                modifier = Modifier.padding(15.dp).fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "To the next screen  ",
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
-                Icon(Icons.Rounded.ArrowForward, contentDescription = "Localized description")
-            }
-        }
-    }
-}
 
-@Composable
-fun PalindromeTest() {
-    Column(
-        modifier = Modifier.padding(top = 75.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
-    ) {
+    ){
         var palindromeTest by remember { mutableStateOf("") }
         OutlinedTextField(
             value = palindromeTest,
             onValueChange = { palindromeTest = it },
-            label = { Text("Insert a palindrome") }
+            label = { Text("Insert a palindrome") },
         )
         var result by remember { mutableStateOf(false) }
         val focusManager = LocalFocusManager.current
@@ -79,7 +41,7 @@ fun PalindromeTest() {
         }
         Box (
             modifier = Modifier.padding(top = 16.dp)
-                ) {
+        ) {
             Text(
                 modifier = Modifier.focusable(),
                 text = "$inputText \nis a palindrome: \n$result",
@@ -88,18 +50,29 @@ fun PalindromeTest() {
 
             )
         }
+    }
 
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+            ) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(1f),
+            onClick = { onNavigateToNext() },
+            shape = RoundedCornerShape(0.dp),
+        ) {
+            Text(
+                text = "To the next screen  ",
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Icon(Icons.Rounded.ArrowForward, contentDescription = "Localized description")
+        }
     }
 }
-
-fun palindromeChecker(inputString: String): Boolean {
-    return inputString == inputString.reversed()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Ahbakken_oblig1Theme {
-        PalindromeTest()
-    }
+fun palindromeChecker(inputString: String): Boolean { // Anna is true with .lowercase()
+    return inputString.lowercase() == inputString.lowercase().reversed()
 }
